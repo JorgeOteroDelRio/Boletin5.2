75b748b175677b7bf8b4dea9f5ddae86cc71c00d
joterodelrio <joterodelrio@atanasoff16.danielcastelao.org>
2015-10-15 12:40:47 +0200

    Commit inicial

---
+++ src/boletin5_2/Boletin5_2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin5_2;
+
+/**
+ *
+ * @author joterodelrio
+ */
+public class Boletin5_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Satelite satelite1 = new Satelite(3.56,234.5,123);
+        satelite1.verPosicion();
+    }
+    
+}
